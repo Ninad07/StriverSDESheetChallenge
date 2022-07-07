d99e3755653ff340108b5251a384010f48491f4c
Ninad07 <55745785+Ninad07@users.noreply.github.com>
2022-07-07 11:45:36 +0530

    Symmetric Tree

---
+++ SymmetricTree.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Solution {
+    public static boolean isSymmetric(BinaryTreeNode<Integer> root) {
+        return isSymmetric(root, root);
+    }
+    
+    private static boolean isSymmetric(BinaryTreeNode<Integer> root1, BinaryTreeNode<Integer> root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(!root1.data.equals(root2.data)) return false;
+        return isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+}
