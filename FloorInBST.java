ee1c852f1cf5c49bbcb6349e690c139340d876d2
Ninad07 <55745785+Ninad07@users.noreply.github.com>
2022-07-08 13:20:18 +0530

    Floor In BST

---
+++ FloorInBST.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Solution {
+
+    public static int floorInBST(TreeNode<Integer> root, int X) {
+        int[] ans = {-1};
+        inorder(root, X, ans);
+        return ans[0];
+    }
+    
+    private static boolean inorder(TreeNode<Integer> root, int x, int[] ans) {
+        if(root == null) return false;
+        if(inorder(root.left, x, ans)) return true;
+        if(root.data <= x) ans[0] = root.data;
+        if(root.data > x) return true;
+        if(inorder(root.right, x, ans)) return true;
+        return false;
+    }
+}
+
